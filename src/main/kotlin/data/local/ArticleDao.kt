6f0b5e15606be7b5321b936f15ccba79ea1d1a1f
FredNekrasov <frednekrasov49@gmail.com>
2024-11-25 22:07:41 +0300

    creating dao classes for working with the database

---
+++ src/main/kotlin/data/local/ArticleDao.kt
@@ -0,0 +1,43 @@
+package data.local
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * ArticleDao is used for working with the database
+ * @param db is an ANTDatabase class
+ */
+class ArticleDao(
+    private val db: ANTDatabase
+) {
+    /**
+     * Get count all articles from the database
+     * @return Long
+     */
+    suspend fun getCountAllArticles(): Long = withContext(Dispatchers.IO) {
+        db.articleQueries.getCountAllArticles().executeAsOne()
+    }
+    /**
+     * Get articles by catalog from the database
+     * @param catalogId is an ID of the catalog
+     * @return List of articles
+     */
+    suspend fun getArticlesByCatalog(catalogId: Int): List<ArticleEntity> = withContext(Dispatchers.IO) {
+        val catalog = db.articleQueries.getAllArticleTypes().executeAsList().getOrNull(catalogId) ?: return@withContext emptyList()
+        db.articleQueries.getArticlesByCatalog(catalog).executeAsList()
+    }
+    /**
+     * Upsert article in the database
+     * @param article is an Article class that will be inserted in the database if it doesn't exist or updated otherwise
+     */
+    suspend fun upsertArticle(article: ArticleEntity) = withContext(Dispatchers.IO) {
+        db.articleQueries.upsertArticle(article)
+    }
+    /**
+     * Delete article from the database
+     * @param id is an ID of the article that will be deleted
+     */
+    suspend fun deleteArticle(id: Long) = withContext(Dispatchers.IO) {
+        db.articleQueries.deleteArticle(id)
+    }
+}
---
+++ src/main/kotlin/data/local/ArticleStatusDao.kt
@@ -0,0 +1,41 @@
+package data.local
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * ArticleStatusDao is used for working with the database
+ * @param db is an ANTDatabase class
+ */
+class ArticleStatusDao(
+    private val db: ANTDatabase
+) {
+    /**
+     * Get all article status from the database
+     * @return List of articles
+     */
+    suspend fun getArticleStatusByCatalogId(catalogId: Long): ArticleStatusEntity? = withContext(Dispatchers.IO) {
+        db.articleStatusQueries.getArticleStatusByCatalogId(catalogId).executeAsOneOrNull()
+    }
+    /**
+     * Get count of all article status from the database
+     * @return Long
+     */
+    suspend fun getCountAllArticleStatus(): Long = withContext(Dispatchers.IO) {
+        db.articleStatusQueries.getCountAllArticleStatus().executeAsOne()
+    }
+    /**
+     * Upsert article status in the database
+     * @param articleStatus is an ArticleStatus class that will be inserted in the database if it doesn't exist or updated otherwise
+     */
+    suspend fun upsertArticleStatus(articleStatus: ArticleStatusEntity) = withContext(Dispatchers.IO) {
+        db.articleStatusQueries.upsertArticleStatus(articleStatus)
+    }
+    /**
+     * Delete article status from the database
+     * @param id is an ID of the article that will be deleted
+     */
+    suspend fun deleteArticleStatus(id: Long) = withContext(Dispatchers.IO) {
+        db.articleStatusQueries.deleteArticleStatus(id)
+    }
+}
