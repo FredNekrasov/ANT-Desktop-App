2d37729a2c4f4e5c8f5d1fbe7a6c8fd5bd1726c3
FredNekrasov <frednekrasov49@gmail.com>
2024-12-15 00:29:19 +0300

    refactoring screens and adapting them to different screen sizes

--- src/main/kotlin/presentation/navigation/MainEP.kt
+++ src/main/kotlin/presentation/navigation/MainEP.kt
@@ -3,8 +3,6 @@ package presentation.navigation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -13,51 +11,44 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import presentation.core.*
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainEntryPoint() {
     val uriHandler = LocalUriHandler.current
     val navItems = ANTStrings.screens
-    val scope = rememberCoroutineScope()
     val controller = rememberNavController()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
     var selectedItemIndex by remember { mutableIntStateOf(0) }
     val navigateTo: (Int, String) -> Unit = { index, route ->
         controller.navigate(route)
         selectedItemIndex = index
-        scope.launch { drawerState.close() }
     }
-    ModalNavigationDrawer(
-        drawerContent = {
-            ModalDrawerSheet {
-                LazyColumn(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-                    itemsIndexed(navItems) { index, route ->
-                        FredNavigationDrawerItem(
-                            text = route,
-                            selected = index == selectedItemIndex,
-                            onClick = {
-                                when(route) {
-                                    ANTStrings.SPIRITUAL_TALKS -> uriHandler.openUri(ANTStrings.SPIRITUAL_TALKS_URL)
-                                    ANTStrings.INFORMATION -> uriHandler.openUri(ANTStrings.INFORMATION_URL)
-                                    else -> navigateTo(index, route)
-                                }
+    val currentRoute = controller.currentBackStackEntryAsState().value?.destination?.route
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        modifier = Modifier.fillMaxSize().padding(WindowInsets.systemBars.asPaddingValues()),
+        topBar = { FredTopAppBar(currentRoute != navItems[2]) { navigateTo(2, navItems[2]) } },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Row(Modifier.fillMaxSize().padding(padding)) {
+            VerticalDivider()
+            LazyColumn(Modifier.fillMaxHeight().fillMaxWidth(0.24f).padding(4.dp)) {
+                itemsIndexed(navItems) { index, route ->
+                    FredNavigationDrawerItem(
+                        text = route,
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            when(route) {
+                                ANTStrings.SPIRITUAL_TALKS -> uriHandler.openUri(ANTStrings.SPIRITUAL_TALKS_URL)
+                                ANTStrings.INFORMATION -> uriHandler.openUri(ANTStrings.INFORMATION_URL)
+                                else -> navigateTo(index, route)
                             }
-                        )
-                        Spacer(Modifier.height(2.dp))
-                    }
+                        }
+                    )
+                    Spacer(Modifier.height(2.dp))
                 }
             }
-        }, drawerState = drawerState) {
-        val currentRoute = controller.currentBackStackEntryAsState().value?.destination?.route
-        val snackbarHostState = remember { SnackbarHostState() }
-        Scaffold(
-            Modifier.fillMaxSize(),
-            topBar = { FredTopAppBar { scope.launch { drawerState.open() } } },
-            floatingActionButton = { if(currentRoute != navItems[2]) FredFloatingActionButton({ navigateTo(2, navItems[2]) }, Icons.Outlined.DateRange) },
-            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
-        ) { padding ->
-            ANTNavHost(controller, snackbarHostState, Modifier.fillMaxSize().padding(WindowInsets.systemBars.asPaddingValues()).padding(padding))
+            VerticalDivider()
+            Box(Modifier.fillMaxSize().padding(8.dp)) { ANTNavHost(controller, snackbarHostState) }
         }
     }
 }
--- src/main/kotlin/presentation/screens/onePages/Schedule.kt
+++ src/main/kotlin/presentation/screens/onePages/Schedule.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.util.fastForEach
 import coil3.compose.AsyncImage
 import presentation.core.ANTStrings
 import presentation.screens.viewModels.ArticleState
@@ -12,8 +13,8 @@ import presentation.screens.viewModels.ArticleState
 @Composable
 fun Schedule(state: ArticleState) {
     Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
-        state.list.forEach {
-            if(it.articleType != ANTStrings.SCHEDULE) return@forEach
+        state.list.fastForEach {
+            if(it.articleType != ANTStrings.SCHEDULE) return@fastForEach
             AsyncImage(model = it.title, contentDescription = it.title, modifier = Modifier.fillMaxWidth())
             AsyncImage(model = it.description, contentDescription = it.description, modifier = Modifier.fillMaxWidth())
         }
