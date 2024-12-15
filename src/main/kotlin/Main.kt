a02e1176e8aeec9d5b5dabdb0371364b648402d6
FredNekrasov <frednekrasov49@gmail.com>
2024-12-15 13:57:19 +0300

    refactoring the Main.kt file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.*
+import coil3.ImageLoader
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import di.dataModule
 import di.presentationModule
+import okhttp3.OkHttpClient
 import org.koin.compose.KoinApplication
 import presentation.navigation.MainEntryPoint
 
@@ -12,18 +17,24 @@ import presentation.navigation.MainEntryPoint
 @Preview
 fun App() {
     MaterialTheme {
+        setSingletonImageLoaderFactory { context ->
+            ImageLoader.Builder(context).components {
+                add(OkHttpNetworkFetcherFactory(OkHttpClient()))
+            }.crossfade(true).logger(DebugLogger()).build()
+        }
         MainEntryPoint()
     }
 }
 
 fun main() = application {
+    val state = rememberWindowState(placement = WindowPlacement.Maximized)
     KoinApplication(
         application = {
             printLogger()
             modules(dataModule, presentationModule)
         }
     ) {
-        Window(onCloseRequest = ::exitApplication) {
+        Window(onCloseRequest = ::exitApplication, state = state) {
             App()
         }
     }
