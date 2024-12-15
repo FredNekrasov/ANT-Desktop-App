516ace69fed035bfc3e8c5f6ee574777d84130e5
FredNekrasov <frednekrasov49@gmail.com>
2024-12-15 11:12:20 +0300

    refactoring the ImageSlider

--- src/main/kotlin/presentation/core/components/ImageSlider.kt
+++ src/main/kotlin/presentation/core/components/ImageSlider.kt
@@ -1,14 +1,16 @@
 package presentation.core.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import coil3.compose.AsyncImage
 import coil3.toUri
 import domain.models.Article
@@ -23,13 +25,23 @@ internal fun ImageSlider(
     article: Article,
     modifier: Modifier = Modifier
 ) {
+    var isDialogVisible by remember { mutableStateOf(false) }
+    var url by remember { mutableStateOf("") }
     LazyRow(modifier, horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically) {
         items(article.content, key = { it }) { photo ->
             AsyncImage(
                 model = photo.toUri(),
                 contentDescription = article.title,
-                Modifier.fillMaxWidth(0.3f).fillMaxHeight(0.3f).border(2.dp, MaterialTheme.colorScheme.background)
+                Modifier.fillMaxHeight().fillMaxWidth().clickable {
+                    url = photo
+                    isDialogVisible = true
+                }.border(2.dp, MaterialTheme.colorScheme.background)
             )
+        }
+    }
+    if(isDialogVisible) Dialog(onDismissRequest = { isDialogVisible = false }) {
+        Column(Modifier.fillMaxSize().padding(8.dp)) {
+            AsyncImage(model = url.toUri(), contentDescription = article.title, Modifier.fillMaxSize())
         }
     }
 }
