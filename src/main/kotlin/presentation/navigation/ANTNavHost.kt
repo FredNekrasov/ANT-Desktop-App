f92fdebee1a6e92e0f7024750a40d636c538539e
FredNekrasov <frednekrasov49@gmail.com>
2024-12-17 16:06:51 +0300

    refactoring the ANTNavHost

--- src/main/kotlin/presentation/navigation/ANTNavHost.kt
+++ src/main/kotlin/presentation/navigation/ANTNavHost.kt
@@ -16,34 +16,45 @@ import org.koin.core.qualifier.qualifier
 import presentation.core.*
 import presentation.screens.*
 import presentation.screens.onePages.*
-import presentation.screens.viewModels.ArticleVM
+import presentation.screens.viewModels.*
 
 @Composable
 fun ANTNavHost(
     controller: NavHostController,
     snackbarHostState: SnackbarHostState,
     modifier: Modifier = Modifier.fillMaxSize(),
-    articleVM: ArticleVM = koinInject(qualifier<ArticleVM>()),
     navItems: SnapshotStateList<String> = ANTStrings.screens
 ) {
-    val state = articleVM.articlesSF.collectAsState().value
+    val mainVM: MainVM = koinInject(qualifier<MainVM>())
+    val parishLifeVM: ParishLifeVM = koinInject(qualifier<ParishLifeVM>())
+    val youthClubVM: YouthClubVM = koinInject(qualifier<YouthClubVM>())
+    val advicesVM: AdvicesVM = koinInject(qualifier<AdvicesVM>())
+    val historyVM: HistoryVM = koinInject(qualifier<HistoryVM>())
+    val storiesVM: StoriesVM = koinInject(qualifier<StoriesVM>())
+    val mainArticleState = mainVM.articlesSF.collectAsState().value
     NavHost(navController = controller, startDestination = navItems[0]) {
-        composable(navItems[0]) { MainScreen(state) }
-        composable(navItems[1]) { ParishLife(state, articleVM::getArticles) }
-        composable(navItems[2]) { Schedule(state) }
-        composable(navItems[3]) { Box(modifier) { CircularProgressIndicator(Modifier.align(Alignment.Center)) } }
-        composable(navItems[4]) { YouthClub(state, articleVM::getArticles) }
-        composable(navItems[5]) { Priesthood(state) }
-        composable(navItems[6]) { Advices(state, articleVM::getArticles) }
-        composable(navItems[7]) { History(state, articleVM::getArticles) }
-        composable(navItems[8]) { Sacraments(state) }
-        composable(navItems[9]) { Box(modifier) { LinearProgressIndicator(Modifier.align(Alignment.Center)) } }
-        composable(navItems[10]) { Volunteerism(state) }
-        composable(navItems[11]) { Stories(state, articleVM::getArticles) }
+        composable(navItems[0]) { MainScreen(mainArticleState) }
+        composable(navItems[1]) { ParishLife(parishLifeVM.articlesSF.collectAsState().value, parishLifeVM::getParishLifeArticles) }
+        composable(navItems[2]) { Schedule(mainArticleState) }
+        composable(navItems[3]) { ANTProgressIndicator(modifier) }
+        composable(navItems[4]) { YouthClub(youthClubVM.articlesSF.collectAsState().value, youthClubVM::getYouthClubArticles) }
+        composable(navItems[5]) { Priesthood(mainArticleState) }
+        composable(navItems[6]) { Advices(advicesVM.articlesSF.collectAsState().value, advicesVM::getAdviceArticles) }
+        composable(navItems[7]) { History(historyVM.articlesSF.collectAsState().value, historyVM::getHistoryArticles) }
+        composable(navItems[8]) { Sacraments(mainArticleState) }
+        composable(navItems[9]) { ANTProgressIndicator(modifier) }
+        composable(navItems[10]) { Volunteerism(mainArticleState) }
+        composable(navItems[11]) { Stories(storiesVM.articlesSF.collectAsState().value, storiesVM::getStoryArticles) }
     }
-    LaunchedEffect(state.status) {
-        articleVM.articlesSF.collectLatest {
-            if(it.status.isError()) snackbarHostState.showSnackbar(it.status.getMessage())
+    var isLoading by remember { mutableStateOf(false) }
+    LaunchedEffect(mainArticleState.status) {
+        mainVM.articlesSF.collectLatest { state ->
+            state.status.displayMessage(snackbarHostState) { isLoading = it }
         }
     }
+    if(isLoading) ANTProgressIndicator(modifier)
+}
+@Composable
+private fun ANTProgressIndicator(modifier: Modifier) {
+    Box(modifier) { CircularProgressIndicator(Modifier.align(Alignment.Center)) }
 }
