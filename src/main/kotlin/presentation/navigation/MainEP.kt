ef712de883d893dbd8fd5679bd10fdb5571dd626
FredNekrasov <frednekrasov49@gmail.com>
2025-01-25 13:17:08 +0300

    extracting the code block from MainEntryPoint function to new ANTNavDrawer function

--- src/main/kotlin/presentation/navigation/MainEP.kt
+++ src/main/kotlin/presentation/navigation/MainEP.kt
@@ -1,20 +1,17 @@
 package presentation.navigation
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import presentation.core.*
+import presentation.core.ANTStrings
+import presentation.core.FredTopAppBar
 
 @Composable
 fun MainEntryPoint() {
-    val uriHandler = LocalUriHandler.current
     val navItems = ANTStrings.screens
     val controller = rememberNavController()
     var selectedItemIndex by remember { mutableIntStateOf(0) }
@@ -31,22 +28,7 @@ fun MainEntryPoint() {
     ) { padding ->
         Row(Modifier.fillMaxSize().padding(padding)) {
             VerticalDivider()
-            LazyColumn(Modifier.fillMaxHeight().fillMaxWidth(0.24f).padding(4.dp)) {
-                itemsIndexed(navItems) { index, route ->
-                    FredNavigationDrawerItem(
-                        text = route,
-                        selected = index == selectedItemIndex,
-                        onClick = {
-                            when(route) {
-                                ANTStrings.SPIRITUAL_TALKS -> uriHandler.openUri(ANTStrings.SPIRITUAL_TALKS_URL)
-                                ANTStrings.INFORMATION -> uriHandler.openUri(ANTStrings.INFORMATION_URL)
-                                else -> navigateTo(index, route)
-                            }
-                        }
-                    )
-                    Spacer(Modifier.height(2.dp))
-                }
-            }
+            ANTNavDrawer(navigateTo, selectedItemIndex)
             VerticalDivider()
             Box(Modifier.fillMaxSize().padding(8.dp)) { ANTNavHost(controller, snackbarHostState) }
         }
